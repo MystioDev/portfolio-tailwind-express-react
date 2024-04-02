4cfe34d50246dc2945aaee5eb5d6e31e1e3617cf
VaradiMarcell <mystio.developer@gmail.com>
2024-04-03 00:52:46 +0200

    Updated Routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+
+import './index.css';
 
 import HomePage from './pages/HomePage'
 import ContactPage from './pages/ContactPage'
 import ReferencesPage from './pages/ReferencesPage'
-import App from './App';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
