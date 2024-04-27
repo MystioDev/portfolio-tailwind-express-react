ac4b5dc9174d7a0457a56bb5463514c7f757c851
VaradiMarcell <mystio.developer@gmail.com>
2024-04-27 16:47:48 +0200

    SideBar Animation Fixed, Responsive

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -5,6 +5,8 @@ import SideBarSocialIcon from "./SideBarSocialIcon";
 const App = () => {
   let isAlertSent = false;
 
+  const [hover, setHover] = useState(false);
+
   return (
     <nav
       onMouseEnter={(e) => {
@@ -22,48 +24,83 @@ const App = () => {
           return;
         }
 
+        setHover(true);
+
         e.target.classList.add("w-64");
         e.target.classList.remove("lg:w-28");
       }}
       onMouseLeave={(e) => {
         e.target.classList.remove("w-64");
         e.target.classList.add("lg:w-28");
+
+        setHover(false);
       }}
       className="group/nav overflow-hidden transition-all fixed left-0 top-0 h-screen w-12 lg:w-28 bg-stone-950 flex justify-between items-center flex-col"
     >
       <img src="/logo1024_transparent.png" alt="Logo" className="w-28 h-auto" />
 
       <ul className="w-auto flex flex-col gap-8 lg:gap-10">
-        <SideBarIcon svgName="HomeIcon" iconLabel="Kezdés" url="/" />
+        <SideBarIcon
+          svgName="HomeIcon"
+          iconLabel="Kezdés"
+          url="/"
+          isHovered={hover}
+          delay="delay-0"
+        />
 
-        <SideBarIcon svgName="AboutIcon" iconLabel="Rólam" url="/" />
+        <SideBarIcon
+          svgName="AboutIcon"
+          iconLabel="Rólam"
+          url="/"
+          isHovered={hover}
+          delay="delay-100"
+        />
 
-        <SideBarIcon svgName="ReferencesIcon" iconLabel="Munkáim" url="/" />
+        <SideBarIcon
+          svgName="ReferencesIcon"
+          iconLabel="Munkáim"
+          url="/"
+          isHovered={hover}
+          delay="delay-200"
+        />
 
-        <SideBarIcon svgName="ContactIcon" iconLabel="Keress fel" url="/" />
+        <SideBarIcon
+          svgName="ContactIcon"
+          iconLabel="Keress fel"
+          url="/"
+          isHovered={hover}
+          delay="delay-300"
+        />
       </ul>
 
-      <div className="mb-32 sm:mb-12 bg-stone-900 rounded-full px-2 lg:px-3 py-3 lg:py-6 flex flex-col  gap-3 group-hover/nav:flex-row group-hover/nav:lg:py-3 group-hover:lg:px-6">
-        <SideBarSocialIcon
-          iconUrl="/discord-icon.svg"
-          iconLabel="Discord elérhetőség"
-          url="/"
-          target="_blank"
-        />
-
-        <SideBarSocialIcon
-          iconUrl="/instagram-icon.svg"
-          iconLabel="Instagra elérhetőség"
-          url="/"
-          target="_blank"
-        />
-
-        <SideBarSocialIcon
-          iconUrl="/mail-icon.svg"
-          iconLabel="E-mail elérhetőség"
-          url="/"
-          target="_blank"
-        />
+      <div className="h-52 flex items-end">
+        <aside
+          className={
+            "transition-all mb-32 sm:mb-12 bg-stone-900 rounded-full px-2 lg:px-3 py-3 lg:py-6 flex flex-col gap-3 " +
+            (hover
+              ? "group-hover/nav:flex-row group-hover/nav:lg:py-3 group-hover:lg:px-6"
+              : "")
+          }
+        >
+          <SideBarSocialIcon
+            iconUrl="/discord-icon.svg"
+            iconLabel="Discord elérhetőség"
+            url="/"
+            target="_blank"
+          />
+          <SideBarSocialIcon
+            iconUrl="/instagram-icon.svg"
+            iconLabel="Instagram elérhetőség"
+            url="/"
+            target="_blank"
+          />
+          <SideBarSocialIcon
+            iconUrl="/mail-icon.svg"
+            iconLabel="E-mail elérhetőség"
+            url="/"
+            target="_blank"
+          />
+        </aside>
       </div>
     </nav>
   );
--- frontend/src/components/SideBarIcon.jsx
+++ frontend/src/components/SideBarIcon.jsx
@@ -4,9 +4,16 @@ const App = (props) => {
   let svgName = props.svgName || "//";
   let url = props.url || "";
   let iconLabel = props.iconLabel || "";
+  let isHovered = props.isHovered || false;
+  let delay = props.delay || "";
 
   return (
-    <li className="flex w-auto group-hover/nav:w-48 items-center justify-end flex-row gap-5 relative">
+    <li
+      className={
+        "flex w-auto items-center justify-center flex-row gap-5 relative transition-all " +
+        (isHovered ? "translate-x-0" : "translate-x-16")
+      }
+    >
       <img
         src="hashtag.svg"
         alt="Hashtag for Hover"
@@ -14,7 +21,7 @@ const App = (props) => {
       />
       <a
         href={url}
-        className="group/link w-full flex items-center justify-center group-hover/nav:justify-start gap-10"
+        className="group/link w-full flex items-center flex-row justify-start gap-10"
       >
         <div className="h-12 w-12 flex items-center justify-center">
           <ImportSvg
@@ -23,7 +30,17 @@ const App = (props) => {
           />
         </div>
 
-        <div className="group/nav text-xl transition-all delay-1000 hidden opacity-0 group-hover/nav:flex group-hover/nav:opacity-100 text-stone-300 font-bold">
+        <div
+          className={
+            "text-xl transition-all " +
+            delay +
+            " " +
+            (isHovered
+              ? "opacity-100 translate-x-0"
+              : "opacity-0 translate-x-10") +
+            " text-stone-300 font-bold"
+          }
+        >
           {iconLabel}
         </div>
       </a>
